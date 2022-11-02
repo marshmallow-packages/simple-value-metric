742fe4914b2fef432036234840d477231ba23eb4
Stef van Esch <stef@marshmallow.dev>
2022-11-02 13:18:04 +0100

    Fixed the help tooltip

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
-    <Card class="flex flex-col relative px-6 py-4">
-        <div class="h-6 flex justify-between items-center mb-4">
-            <h3 class="mr-3 leading-tight text-sm font-bold">
+    <Card class="relative flex flex-col px-6 py-4">
+        <div class="flex items-center justify-between h-6 mb-4">
+            <h3 class="mr-3 text-sm font-bold leading-tight">
                 {{ card.title }}
             </h3>
             <div
@@ -14,39 +14,29 @@
                     class="w-full px-2.5 py-1.5 border-2 border-primary-300 shadow-sm text-xs font-medium rounded text-primary-500 bg-white hover:bg-primary-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
                     :class="card.button_class"
                 >
-                    <span class="w-full block text-center">
+                    <span class="block w-full text-center">
                         {{ card.button_text }}
                     </span>
                 </a>
             </div>
         </div>
-        <div v-if="card.helpText" class="absolute pin-r pin-b p-2 z-50">
-            <tooltip trigger="click">
-                <icon
-                    type="help"
-                    viewBox="0 0 17 17"
-                    height="16"
-                    width="16"
-                    class="cursor-pointer text-60 -mb-1"
-                />
-                <tooltip-content
-                    slot="content"
-                    v-html="card.helpText"
-                    :max-width="card.helpWidth"
-                />
-            </tooltip>
-        </div>
-        <p class="flex items-center text-4xl mb-4">
+        <HelpTextTooltip
+            v-if="card.helpText"
+            :text="card.helpText"
+            :width="card.helpWidth"
+        />
+
+        <p class="flex items-center mb-4 text-4xl">
             {{ card.formattedValue }}
             <span v-if="card.suffix" class="ml-2 text-sm font-bold text-80">
                 {{ card.suffix }}
             </span>
         </p>
         <div class="flex items-center">
-            <p class="text-80 font-bold">
+            <p class="font-bold text-80">
                 <svg
                     v-if="card.footerIcon == 'Decrease'"
-                    class="text-danger fill-current mr-2"
+                    class="mr-2 fill-current text-danger"
                     width="20"
                     height="12"
                 >
@@ -56,7 +46,7 @@
                 </svg>
                 <svg
                     v-if="card.footerIcon == 'Increase'"
-                    class="rotate-180 text-success fill-current mr-2"
+                    class="mr-2 rotate-180 fill-current text-success"
                     width="20"
                     height="12"
                 >
